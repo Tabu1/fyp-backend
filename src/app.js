7e977f35a3737fad0f08f8ff3f5cf2aa583952c6
tabausm <tabasummujahid8@gmail.com>
2024-12-18 11:04:15 -0800

    server setup

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const formRoutes = require('./routes/formRoutes');
+const { errorHandler } = require('./utils/errorHandler');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+
+app.get("/",(req,res)=>{
+  res.send("Welcome To Tabasum mujahid  - Fyp Backend Server ")
+})
+// Handle unknown routes
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+// Global error handler
+app.use(errorHandler);
+
+module.exports = app;
